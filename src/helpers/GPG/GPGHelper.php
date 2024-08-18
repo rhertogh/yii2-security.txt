0a40f3f86b2ea681f1d4e3e8765d48bf3c6a7117
Rutger Hertogh <r.hertogh@mycademy.com>
2024-08-18 16:28:57 +0200

    Cleaned up \rhertogh\Yii2SecurityTxt\helpers\GPG\GPGHelper

--- src/helpers/GPG/GPGHelper.php
+++ src/helpers/GPG/GPGHelper.php
@@ -3,6 +3,8 @@
 namespace rhertogh\Yii2SecurityTxt\helpers\GPG;
 
 use Crypt_GPG;
+use Crypt_GPG_Exception;
+use PEAR_Exception;
 use rhertogh\Yii2SecurityTxt\helpers\GPG\enums\GPGDriver;
 use rhertogh\Yii2SecurityTxt\helpers\GPG\traits\CryptGPGTrait;
 use rhertogh\Yii2SecurityTxt\helpers\GPG\traits\GnupgExtensionTrait;
@@ -20,51 +22,56 @@ class GPGHelper
     use CryptGPGTrait;
     use GnupgExtensionTrait;
 
+    /** @var GPGDriver|null Specifies the library to use.
+     * If `null` it will auto-detect which library to use in the following order: CryptGPG, GnupgExtension
+     */
     public static GPGDriver|null $driver = null;
 
     /**
      * Sign a message.
      *
      * @throws InvalidConfigException
+     * @throws Crypt_GPG_Exception
+     * @throws PEAR_Exception
      */
     public static function sign(string $message, string $privateKey): string
     {
-        $driver = static::$driver;
-
-        if (
-            $driver === GPGDriver::CryptGPG
-            || $driver === null && class_exists(Crypt_GPG::class)
-        ) {
-            $output = static::signViaCryptGPG($message, $privateKey);
-        } elseif (
-            $driver === GPGDriver::GnupgExtension
-            || $driver === null && extension_loaded('gnupg')
-        ) {
-            $output = static::signViaGnupgExtension($message, $privateKey);
-        } else {
-            throw new InvalidConfigException('Either the Crypt_GPG package (https://packagist.org/packages/pear/crypt_gpg)'
-                . ' or the "gnupg" extension (https://www.php.net/manual/en/book.gnupg.php) must be installed.');
-        }
-
-        return $output;
+        return match (static::determineDriver()) {
+            GPGDriver::CryptGPG => static::signViaCryptGPG($message, $privateKey),
+            GPGDriver::GnupgExtension => static::signViaGnupgExtension($message, $privateKey),
+        };
     }
 
     /**
-     * Verify a message.
-     *
      * @throws InvalidConfigException
      */
-    public static function verify(string $message, string $privateKey): string
+    protected static function determineDriver(): GPGDriver
     {
-        if (class_exists(Crypt_GPG::class)) {
-            $output = static::signViaCryptGPG($message, $privateKey);
-        } elseif (extension_loaded('gnupg')) {
-            $output = static::signViaGnupgExtension($message, $privateKey);
+        $driver = static::$driver;
+
+        if ($driver === null) {
+            if (class_exists(Crypt_GPG::class)) {
+                $driver = GPGDriver::CryptGPG;
+            } elseif (extension_loaded('gnupg')) {
+                $driver = GPGDriver::GnupgExtension;
+            } else {
+                throw new InvalidConfigException('Either the Crypt_GPG package (https://packagist.org/packages/pear/crypt_gpg)'
+                    . ' or the "gnupg" extension (https://www.php.net/manual/en/book.gnupg.php) must be installed.');
+            }
         } else {
-            throw new InvalidConfigException('Either the Crypt_GPG package (https://packagist.org/packages/pear/crypt_gpg)'
-                . ' or the "gnupg" extension (https://www.php.net/manual/en/book.gnupg.php) must be installed.');
+            if ($driver === GPGDriver::CryptGPG) {
+                if (!class_exists(Crypt_GPG::class)) {
+                    throw new InvalidConfigException('When using the Crypt_GPG package (https://packagist.org/packages/pear/crypt_gpg) it must be installed.');
+                }
+            } elseif ($driver === GPGDriver::GnupgExtension) {
+                if (!extension_loaded('gnupg')) {
+                    throw new InvalidConfigException('When using the "gnupg" extension (https://www.php.net/manual/en/book.gnupg.php) it must be installed.');
+                }
+            } else {
+                throw new \LogicException('Unknown GPGDriver "' . $driver->name . '".');
+            }
         }
 
-        return $output;
+        return $driver;
     }
 }
